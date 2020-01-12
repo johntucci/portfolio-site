45a9f4aace5161a1365270232b38973889e9fb41
John Santucci <johnsantucci98@gmail.com>
2020-01-12 13:25:15 -0600

    improved About Me layout, added some flare

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ProfileImage from './img/profile.jpg';
 import ContactForm from './components/ContactForm';
 import ProjectCard from './components/ProjectCard';
 
-import { Container, Navbar, Nav, Jumbotron, Media, Row } from 'react-bootstrap';
+import { Container, Navbar, Nav, Jumbotron, Row } from 'react-bootstrap';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -20,16 +20,19 @@ const config = {
    firstName: 'Nick',
    lastName: 'Santucci',
    accentColor: '#4bb7ff',
-   animationsFireOnce: false
+   animationsFireOnce: true
 };
 
 function NavBar() {
    return (
-      <Navbar expand="lg">
+      <Navbar
+         expand="lg"
+         style={{ position: 'absolute', background: '#ffffff', width: '100%' }}
+      >
          <Navbar.Brand
             href="#landing"
             style={{
-               fontFamily: 'Courier New, Monospace',
+               // fontFamily: 'Courier New, Monospace',
                fontSize: '1.5rem'
             }}
          >
@@ -109,8 +112,8 @@ function ParallaxImage(props) {
             width: '100%',
 
             backgroundAttachment: 'fixed',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
+            // backgroundPosition: 'center',
+            backgroundRepeat: props.repeat ? 'repeat' : 'no-repeat',
             backgroundSize: 'cover'
          }}
       />
@@ -119,7 +122,7 @@ function ParallaxImage(props) {
 
 function Logo() {
    return (
-      <span>
+      <span className="logo">
          &lt;{' '}
          <span style={{ color: config.accentColor }}>
             {config.firstName} {config.lastName}
@@ -142,27 +145,58 @@ function Spacer({ height = '6rem' }) {
 
 function AboutMe() {
    return (
-      <Media className="my-5">
-         <img
-            width={200}
-            height={200}
-            src={ProfileImage}
-            alt="My face"
-            className="mr-3 shadow"
-            style={{ borderRadius: '.25rem' }}
-            data-aos="fade-right"
-         />
-         <Media.Body className="text-center" data-aos="fade-up">
-            <h5>Nice To Meet You</h5>
-            <p>
-               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-               scelerisque ante sollicitudin commodo. Cras purus odio,
-               vestibulum in vulputate at, tempus viverra turpis. Fusce
-               condimentum nunc ac nisi vulputate fringilla. Donec lacinia
-               congue felis <a href="/">[More ...]</a>
-            </p>
-         </Media.Body>
-      </Media>
+      <div className="container-fluid justify-content-center">
+         <div className="row">
+            <div className="col-sm-auto col-xs-12 text-center">
+               <img
+                  src={ProfileImage}
+                  alt="My face"
+                  className="shadow"
+                  style={{
+                     borderRadius: '.25rem',
+                     width: '100%',
+                     maxWidth: '200px'
+                  }}
+                  data-aos="fade-right"
+               />
+            </div>
+            <div className="col-sm col-xs-12 my-4" data-aos="fade-up">
+               <span className="text-center">
+                  <h5>Nice To Meet You</h5>
+                  <p>
+                     Cras sit amet nibh libero, in gravida nulla. Nulla vel
+                     metus scelerisque ante sollicitudin commodo. Cras purus
+                     odio, vestibulum in vulputate at, tempus viverra turpis.
+                     Fusce condimentum nunc ac nisi vulputate fringilla. Donec
+                     lacinia congue felis.
+                  </p>
+               </span>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+function Dot(props) {
+   const even = {
+      // border: '.3rem solid ' + config.accentColor
+      background: '#fff'
+   };
+   const odd = {
+      background: config.accentColor
+   };
+   return (
+      <div
+         style={{
+            width: '4rem',
+            height: '4rem',
+            borderRadius: '100%',
+            ...(props.order % 2 === 0 ? even : odd)
+         }}
+         className="m-4 shadow"
+         data-aos={props.anim ? props.anim : 'zoom-in'}
+         data-aos-delay={200 * props.order}
+      />
    );
 }
 
@@ -171,8 +205,8 @@ export default class App extends Component {
       AOS.init({
          delay: 100,
          once: config.animationsFireOnce,
-         duration: 600,
-         disable: 'mobile'
+         duration: 600
+         // disable: 'mobile'
       });
    }
    componentWillUpdate() {
@@ -185,52 +219,65 @@ export default class App extends Component {
             <MainHero />
             <div className="mb-4 container-xl">
                <Spacer height="10vh" />
-               <div id="about-me"></div>
-               <Title l1="Here's a little bit" l2="About me" />
-               <Spacer height="10vh" />
+               <div style={{ minHeight: '95vh' }}>
+                  <div id="about-me"></div>
+                  <Title l1="Here's a little bit" l2="About me" />
+                  <Spacer height="20vh" />
 
-               <AboutMe />
+                  <AboutMe />
 
-               <Spacer height="15vh" />
+                  <Spacer height="25vh" />
+                  <div className="d-flex justify-content-around">
+                     <div className="flex-grow-1 d-flex justify-content-center">
+                        <Dot order={1} anim="fade-right" />
+                        <Dot order={2} />
+                        <Dot order={3} anim="fade-left" />
+                     </div>
+                  </div>
+               </div>
 
-               <div id="my-projects"></div>
+               <div style={{ minHeight: '105vh' }}>
+                  <Spacer height="15vh" />
 
-               <Title l1="These Are" l2="My Projects" />
-               <Spacer height="8vh" />
+                  <div id="my-projects"></div>
 
-               <Row className="py-5 row-cols-1 row-cols-sm-2 row-cols-lg-3">
-                  <ProjectCard
-                     name="My Portfolio"
-                     link="#landing"
-                     gitLink="https://github.com"
-                     techStack={['react', 'bootstrap']}
-                     image={LaptopImage}
-                     samePage={true}
-                  />
-                  <ProjectCard
-                     name="E-Commerce"
-                     link="https://example.com"
-                     gitLink="https://github.com"
-                     techStack={['react', 'express', 'mongodb']}
-                     image={ShoppingImage}
-                  />
+                  <Title l1="These Are" l2="My Projects" />
+                  <Spacer height="8vh" />
 
-                  <ProjectCard
-                     name="Admin Panel"
-                     link="https://example.com"
-                     gitLink="https://github.com"
-                     techStack={['express', 'passport', 'mysql']}
-                     image={GraphsImage}
-                  />
-               </Row>
+                  <Row className="py-5 row-cols-1 row-cols-sm-2 row-cols-lg-3">
+                     <ProjectCard
+                        name="My Portfolio"
+                        link="#landing"
+                        gitLink="https://github.com"
+                        techStack={['react', 'bootstrap']}
+                        image={LaptopImage}
+                        samePage={true}
+                     />
+                     <ProjectCard
+                        name="E-Commerce"
+                        link="https://example.com"
+                        gitLink="https://github.com"
+                        techStack={['react', 'express', 'mongodb']}
+                        image={ShoppingImage}
+                     />
+
+                     <ProjectCard
+                        name="Admin Panel"
+                        link="https://example.com"
+                        gitLink="https://github.com"
+                        techStack={['express', 'passport', 'mysql']}
+                        image={GraphsImage}
+                     />
+                  </Row>
+               </div>
             </div>
-            <div id="contact-me"></div>
             <ParallaxImage src={ContactMeImage} />
+            <div id="contact-me"></div>
             <Title l1="Interested? You can" l2="Contact me" />
 
             <div
                className="container-lg"
-               data-aos="flip-up"
+               data-aos="fade-down"
                data-aos-delay="500"
             >
                <ContactForm />
@@ -238,7 +285,10 @@ export default class App extends Component {
             <Spacer height="15vh" />
 
             <Container fluid className="text-center mb-4" data-aos="zoom-in">
-               <h1>
+               <h1
+                  onClick={() => window.scrollTo(0, 0)}
+                  style={{ cursor: 'pointer' }}
+               >
                   <Logo />
                   <Underline />
                </h1>
